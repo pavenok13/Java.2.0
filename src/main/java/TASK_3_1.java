e6b0b7dbdaf4f7e70a680d8f1f94b037427fccb0
yota <yas-07@mail.ru>
2019-10-10 10:18:54 +0300

    Задача 3.1.

---
+++ src/main/java/TASK_3_1.java
@@ -0,0 +1,43 @@
+/**
+ * @author Stolyarchuk Andrey
+ */
+
+public class TASK_3_1 {
+    public static void main(String[] args) {
+        int intArr[] = new int[21];
+        int max = intArr[0];
+        int min = intArr[0];
+        int posmin1 = 0;
+        int posmax2 = 0;
+        for (int i = 0; i < intArr.length; i++) {
+            intArr[i] = ((int) (Math.random() * 20) - 10);
+            System.out.println("Элемент массива номер " + i + " равен: " + intArr[i]);
+        }
+        for (int i = 0; i < intArr.length; i++) {
+            if (intArr[i] < 0) {
+                min = intArr[i];
+            }
+            if (intArr[i] > 0) {
+                max = intArr[i];
+            }
+        }
+        for (int i = 0; i < intArr.length; i++) {
+            if (intArr[i] < min) {
+                min = intArr[i];
+                posmin1 = i;
+            }
+            if (intArr[i] < max & intArr[i] > 0) {
+                max = intArr[i];
+                posmax2 = i;
+            }
+        }
+        System.out.println("Максимальный отрицательный эл-т массива: " + min);
+        System.out.println("Минимальный положительный эл-т массива: " + max);
+        intArr[posmin1] = max;
+        intArr[posmax2] = min;
+        System.out.println("Новый массив (поменяли местами максимальный отрицательный и минимальный положительный эл-ты): " + posmin1 + " и " + posmax2);
+        for (int i = 0; i < intArr.length; i++) {
+            System.out.println("Элементт массива номер " + i + " равен: " + intArr[i]);
+        }
+    }
+}
