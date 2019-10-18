3df29c15cc22e7df5c3ee74b6ee079e3f5ce8a64
yota <yas-07@mail.ru>
2019-10-18 09:47:11 +0300

    Исправил выбор максимально отрицательного элемента.

--- src/main/java/TASK_3_1.java
+++ src/main/java/TASK_3_1.java
@@ -22,7 +22,7 @@ public class TASK_3_1 {
             }
         }
         for (int i = 0; i < intArr.length; i++) {
-            if (intArr[i] < 0) {
+            if (intArr[i] > min & intArr[i] < 0) {
                 min = intArr[i];
                 posmin1 = i;
             }
