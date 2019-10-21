1ee3dd0c2a401e56bf170730aef125d5cfca7c47
yota <yas-07@mail.ru>
2019-10-21 12:45:55 +0300

    Добавил юнит тест деление на 0.

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,15 +2,18 @@ import TASK_4.Delenie;
 import TASK_4.Slozhenie;
 import TASK_4.Umnozhenie;
 import TASK_4.Vichitanie;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.fail;
 
 /**
  * @author Stolyarchuk Andrey
  */
 
 public class CalculatorTest {
+
     @Test
     public void doSum() {
         Slozhenie slozhenie = new Slozhenie(8, 4.5, 1);
@@ -34,14 +37,17 @@ public class CalculatorTest {
 
     @Test
     public void doUmn() {
-        Umnozhenie umnozhenie = new Umnozhenie(3.8, 5,4);
+        Umnozhenie umnozhenie = new Umnozhenie(3.8, 5, 4);
         double res = umnozhenie.doUmn();
         assertEquals("Не прошла проверка оператора умножения", 19.0, res);
     }
-    @Test
-    public void IOException () {
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivideByZero() {
         Delenie delenie = new Delenie(84, 0, 3);
         double res = delenie.doDel();
-        assertEquals("Не прошла проверка оператора деления", "Ошибка, делить на 0 нельзя" , res);
+        try {
+            Assert.fail("Не прошла проверка оператора " + res);
+        } catch(ArithmeticException e ) { }
     }
 }
